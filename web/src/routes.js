72503067e5c0930eb92aa01078d64c88df0d440d
André Barbeto <coaladespreocupado@gmail.com>
2022-12-12 21:46:27 -0300

    algo

--- web/src/routes.js
+++ web/src/routes.js
@@ -3,8 +3,6 @@ import { Route, Routes as MainRoutes } from "react-router-dom";
 
 import Cliente from './pages/Cliente';
 import Home from './pages/Home';
-import Vendas from './pages/Vendas';
-import HistoricoVendas from './pages/HistoricoVendas';
 import CadastroProdutos from './pages/CadastroProdutos';
 import Devolucao from './pages/Devolucao';
 
@@ -12,10 +10,7 @@ export default function Routes(){
   return(
     <MainRoutes>
         <Route path="/" element={<Home />} />
-        <Route path="/vendas" element={<Vendas />} />
-        <Route path="/produtos" element={<Vendas />} />
         <Route path="/cliente" element={<Cliente />} />
-        <Route path="/historico-vendas" element={<HistoricoVendas />} />
         <Route path="/cadastro-produtos" element={<CadastroProdutos />} />
         <Route path="/devolucao" element={<Devolucao />} />
     </MainRoutes>
